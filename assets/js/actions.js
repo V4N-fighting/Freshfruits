81eefff5cd40c3e2d2bc9d51fd65d3250f54f21b
V4N-fighting <ngocvanwjtch@gmail.com>
2024-11-09 21:04:33 +0700

    handling blog element

--- assets/js/actions.js
+++ assets/js/actions.js
@@ -1,8 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
     var splide = new Splide('#image-slide', {
-        type  : 'slide',
-        rewind: true,
-        speed: 500     // Tốc độ chuyển (ms), có thể tùy chỉnh cho mượt mà
+        type: 'loop',       // Chuyển đổi slide lặp lại
+        perPage: 1,         // Số lượng slide hiển thị cùng một lúc
+        autoplay: true,     // Tự động chuyển slide
+        interval: 3000,     // Thời gian giữa các slide (ms)
+        arrows: true,       // Hiển thị nút điều hướng trái và phải
+        pagination: true 
         });
     splide.mount();
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    var splide = new Splide( '#test', {
+        type   : 'loop',
+        perPage: 3,
+        perMove: 1,
+      } );
+      
+      splide.mount();
+});
+
