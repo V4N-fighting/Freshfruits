517fe8f7254de49bfecfeb933e7fdb695d9b8101
V4N-fighting <ngocvanwjtch@gmail.com>
2024-11-10 15:03:54 +0700

    complete invesment element

--- assets/js/actions.js
+++ assets/js/actions.js
@@ -1,3 +1,5 @@
+
+
 document.addEventListener('DOMContentLoaded', function () {
     var splide = new Splide('#image-slide', {
         type: 'loop',       // Chuyển đổi slide lặp lại
@@ -10,14 +12,46 @@ document.addEventListener('DOMContentLoaded', function () {
     splide.mount();
 });
 
+// splide invesment
+
 document.addEventListener('DOMContentLoaded', function () {
-    var splide = new Splide( '#test', {
+    var splideInvesment = new Splide( '#invesment', {
+        type   : 'loop',
+        perPage: 5,
+        perMove: 1,
+        autoplay  : true,      
+        interval  : 1000, 
+      } );
+      
+      splideInvesment.mount();
+});
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     var splideInvesment = new Splide('#invesment', {
+//       type      : 'loop',    // Slide chạy liên tục
+//       autoplay  : true,      // Bật tự động phát
+//       interval  : 3000,      // Thời gian giữa các slide là 3 giây
+//       pauseOnHover: false,   // Không dừng khi hover
+//       perPage   : 5,         // Hiển thị 3 slide một lúc
+//       gap       : '1rem',    // Khoảng cách giữa các slide
+//       arrows    : true,      // Hiển thị mũi tên điều hướng
+//     });
+
+//     splideInvesment.mount();
+//   });
+
+
+// splide blog
+document.addEventListener('DOMContentLoaded', function () {
+    var splide = new Splide( '#blog', {
         type   : 'loop',
         perPage: 3,
         perMove: 1,
-        height: '3'
+        autoplay  : true,      
+        interval  : 1000, 
       } );
       
       splide.mount();
 });
+
 
